34bacaf5c8e47f4a64270bf4126a5c4f896f02b4
Andres <eldersoto1876@hotmail.com>
2022-01-25 19:28:12 -0300

    segundo commit de prueba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fernando <code>src/App.js</code> and save to reload.
+          Fernando Soto <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
